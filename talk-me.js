58ea44872cf0b0b920442d7ebbfc39ccae011a15
ellis-ttt <ellis-ttt@yandex.ru>
2025-11-22 22:23:43 +0300

    wait for element

--- talk-me.js
+++ talk-me.js
@@ -6,11 +6,34 @@
 		return
 	}
 
-	addButton(
-		await getTalkMeUrl(
-			makeUserQuery()
+	function waitForElement(selector) {
+		return new Promise(resolve => {
+			if (document.querySelector(selector)) {
+				return resolve(document.querySelector(selector));
+			}
+
+			const observer = new MutationObserver(mutations => {
+				if (document.querySelector(selector)) {
+					observer.disconnect();
+					resolve(document.querySelector(selector));
+				}
+			});
+
+			observer.observe(document.body, {
+				childList: true,
+				subtree: true
+			});
+		});
+	}
+
+	waitForElement('.user-info')
+		.then(
+			_ => {
+				getTalkMeUrl(
+					makeUserQuery()
+				).then(addButton)
+			}
 		)
-	)
 
 	async function getTalkMeUrl(userQuery) {
 		if (userQuery == null) {
